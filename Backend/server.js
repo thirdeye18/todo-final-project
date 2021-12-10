40725689226ad7b7140c30f8c9b653a061846c3a
Alan-SDE <apotsy8251@gmail.com>
2021-12-10 16:03:43 -0500

    Justin debugged routes on Back-end

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,12 @@
 //server.js
+//for storing values in .env file and retrieving them
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const app = express();
 const path = require('path');
-const bodyParser= require('body-parser');
 const router = require('./routes/index');
 const cors = require('cors');
-
-//for storing values in .env file and retrieving them
-const dotenv = require('dotenv');
-dotenv.config({path:'./.env'});
 
 const port = 3000;
 
@@ -17,9 +15,8 @@ const port = 3000;
 // ========================
 
 //make sure urlencoded before CRUD handelers
-// app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(cors());
 
